a98e299184129876b5625dfafaa56b161ac99e9b
Alexey Yuzhakov <sibprogrammer@gmail.com>
2021-07-22 11:39:59 +0700

    Add generic support for PHP-based apps

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,10 +3,56 @@ package server
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"os/exec"
 )
 
+const defaultIp = "127.0.0.1"
+const defaultPort = "8080"
+
 func Run() error {
-	port := "8080"
-	fmt.Println("Static server started:", "http://127.0.0.1:"+port)
+	ip := defaultIp
+	port := defaultPort
+
+	if fileExists("composer.json") {
+		return runPHP(ip, port)
+	}
+
+	return runStatic(ip, port)
+}
+
+func fileExists(name string) bool {
+	if _, err := os.Stat(name); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
+
+func runCommand(name string, arg ...string) error {
+	command := exec.Command(name, arg...)
+	command.Stdin = os.Stdin
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+	return command.Run()
+}
+
+func runPHP(ip string, port string) error {
+	address := fmt.Sprintf("%s:%s", ip, port)
+	args := []string{"-S", address}
+
+	if fileExists("public") {
+		args = append(args, "-t", "public")
+	} else if fileExists("web") {
+		args = append(args, "-t", "web")
+	}
+
+	return runCommand("php", args...)
+}
+
+func runStatic(ip string, port string) error {
+	address := fmt.Sprintf("http://%s:%s", ip, port)
+	fmt.Println("Static server started:", address)
 	return http.ListenAndServe(":"+port, http.FileServer(http.Dir(".")))
 }
