87d1d0a0c1a25979ee1d535786a08c45662a3f66
Alexey Yuzhakov <sibprogrammer@gmail.com>
2021-08-24 19:24:34 +0700

    Add an ability to run the daemon in the background mode

--- cmd/root.go
+++ cmd/root.go
@@ -2,16 +2,51 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/sevlyar/go-daemon"
 	"github.com/sibprogrammer/uws/internal/server"
 	"github.com/spf13/cobra"
+	"log"
 	"os"
+	"os/signal"
+	"path"
+	"path/filepath"
+	"syscall"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "uws",
 	Short: "Universal Web Server for development purposes",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return server.Run()
+		daemonMode, err := cmd.Flags().GetBool("daemon")
+		if err != nil {
+			return err
+		}
+
+		done := make(chan os.Signal, 1)
+		signal.Notify(done, syscall.SIGINT, syscall.SIGTERM)
+
+		if daemonMode {
+			workDir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
+			context := getDaemonContext("uws", workDir)
+
+			d, err := context.Reborn()
+			if err != nil {
+				log.Fatal("Unable to run: ", err)
+			}
+			if d != nil {
+				fmt.Println("The daemon has been started in the background mode.")
+				return nil
+			}
+			defer context.Release()
+		}
+
+		var result error
+		go func() {
+			result = server.Run()
+		}()
+		<-done
+
+		return result
 	},
 }
 
@@ -20,8 +55,21 @@ func Execute() {
 		versionCmd,
 	)
 
+	rootCmd.PersistentFlags().BoolP("daemon", "d", false, "Run in the background mode")
+
 	if err := rootCmd.Execute(); err != nil {
 		_, _ = fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
+	}
+}
+
+func getDaemonContext(binaryName string, workDir string) *daemon.Context {
+	return &daemon.Context{
+		PidFileName: path.Join(workDir, binaryName+".pid"),
+		PidFilePerm: 0644,
+		LogFileName: path.Join(workDir, binaryName+".log"),
+		LogFilePerm: 0640,
+		WorkDir:     workDir,
+		Umask:       027,
 	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/sibprogrammer/uws
 
 go 1.16
 
-require github.com/spf13/cobra v1.2.1 // indirect
+require (
+	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0 // indirect
+	github.com/sevlyar/go-daemon v0.1.5
+	github.com/spf13/cobra v1.2.1
+)
